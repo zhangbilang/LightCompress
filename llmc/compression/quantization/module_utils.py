9de6db6c0872bca6afd6819534fc106db9d80d28
gushiqiao <gushiqiao@sensetime.com>
2025-07-21 10:57:01 +0000

    Support load from quantized weights

--- llmc/compression/quantization/module_utils.py
+++ llmc/compression/quantization/module_utils.py
@@ -8,7 +8,6 @@ import torch.nn.functional as F
 from loguru import logger
 from transformers.pytorch_utils import ALL_LAYERNORM_LAYERS
 
-from .quant import FloatQuantizer
 from .utils import is_fp8_supported_gpu
 
 if is_fp8_supported_gpu():
@@ -23,6 +22,10 @@ else:
         'Using LLMC Quantizer implementation instead.'
     )
 
+try:
+    from vllm import _custom_ops as ops
+except ModuleNotFoundError:
+    ops = None
 
 try:
     import fast_hadamard_transform
@@ -33,8 +36,6 @@ except Exception:
         'fast_hadamard_transform not installed. '
         'If you need it, please install it firstly.'
     )
-
-from .utils import calculate_zeros_width
 
 
 def block_wise_fp8_forward_func(x, w, w_scale, block_size, bias):
@@ -127,13 +128,105 @@ class LlmcWanTransformerBlock(nn.Module):
         return new_module
 
 
+class VllmQuantLinearInt8(nn.Module):
+    def __init__(self, in_features, out_features, bias=True):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+
+        self.register_buffer('weight', torch.empty((out_features, in_features), dtype=torch.int8))
+        self.register_buffer('weight_scale', torch.empty((out_features, 1), dtype=torch.float32))
+
+        if bias:
+            self.register_buffer('bias', torch.empty(out_features, dtype=torch.bfloat16))
+        else:
+            self.register_buffer('bias', None)
+
+    def act_quant_func(self, x):
+        input_tensor_quant, input_tensor_scale, _ \
+            = ops.scaled_int8_quant(x, scale=None, azp=None, symmetric=True)
+        return input_tensor_quant, input_tensor_scale
+
+    def forward(self, input_tensor):
+        input_tensor = input_tensor.squeeze(0)
+        shape = (input_tensor.shape[0], self.weight.shape[0])
+        dtype = input_tensor.dtype
+        device = input_tensor.device
+        output_tensor = torch.empty(shape, dtype=dtype, device=device, requires_grad=False)
+
+        input_tensor_quant, input_tensor_scale = self.act_quant_func(input_tensor)
+        torch.ops._C.cutlass_scaled_mm(
+            output_tensor,
+            input_tensor_quant,
+            self.weight.t(),
+            input_tensor_scale,
+            self.weight_scale.float(),
+            self.bias,
+        )
+        return output_tensor.unsqueeze(0)
+
+    @classmethod
+    @torch.no_grad()
+    def new(cls, module):
+        in_features = module.in_features
+        out_features = module.out_features
+        bias = module.bias is not None
+        new_module = cls(in_features, out_features, bias)
+        return new_module
+
+
+class VllmQuantLinearFp8(nn.Module):
+    def __init__(self, in_features, out_features, bias=True):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.register_buffer('weight', torch.empty((out_features, in_features), dtype=torch.float8_e4m3fn)) # noqa
+        self.register_buffer('weight_scale', torch.empty((out_features, 1), dtype=torch.float32))
+        if bias:
+            self.register_buffer('bias', torch.empty(out_features, dtype=torch.bfloat16))
+        else:
+            self.register_buffer('bias', None)
+
+    def act_quant_func(self, x):
+        input_tensor_quant, input_tensor_scale \
+            = ops.scaled_fp8_quant(x, None, scale_ub=None, use_per_token_if_dynamic=True)
+        return input_tensor_quant, input_tensor_scale
+
+    def forward(self, input_tensor):
+        input_tensor = input_tensor.squeeze(0)
+        shape = (input_tensor.shape[0], self.weight.shape[0])
+        dtype = input_tensor.dtype
+        device = input_tensor.device
+        output_tensor = torch.empty(shape, dtype=dtype, device=device, requires_grad=False)
+        input_tensor_quant, input_tensor_scale = self.act_quant_func(input_tensor)
+        torch.ops._C.cutlass_scaled_mm(
+            output_tensor,
+            input_tensor_quant,
+            self.weight.t(),
+            input_tensor_scale,
+            self.weight_scale.float(),
+            self.bias,
+        )
+
+        return output_tensor.unsqueeze(0)
+
+    @classmethod
+    @torch.no_grad()
+    def new(cls, module):
+        in_features = module.in_features
+        out_features = module.out_features
+        bias = module.bias is not None
+        new_module = cls(in_features, out_features, bias)
+        return new_module
+
+
 class LlmcFp8Linear(nn.Module):
     def __init__(self, in_features, out_features, bias, block_size):
         super().__init__()
         self.block_size = block_size
         self.in_features = in_features
         self.out_features = out_features
-        if bias is not None:
+        if bias:
             self.bias = nn.Parameter(torch.empty(out_features))
         else:
             self.register_parameter('bias', None)
@@ -172,7 +265,7 @@ class LlmcFp8Linear(nn.Module):
     def new(cls, module, block_size):
         in_features = module.in_features
         out_features = module.out_features
-        bias = module.bias
+        bias = module.bias is not None
         new_module = cls(in_features, out_features, bias, block_size)
         return new_module
 
--- llmc/compression/quantization/utils.py
+++ llmc/compression/quantization/utils.py
@@ -5,21 +5,6 @@ def make_divisible(c, divisor):
     return (c + divisor - 1) // divisor
 
 
-def calculate_zeros_width(in_features, group_size=128, pack_num=8):
-    if group_size >= 128:
-        size_multiplier = 1
-    elif group_size == 64:
-        size_multiplier = 2
-    elif group_size == 32:
-        size_multiplier = 4
-    else:
-        raise NotImplementedError
-
-    base_width = make_divisible(in_features // group_size, pack_num)
-    base_width = make_divisible(base_width, size_multiplier) * size_multiplier
-    return base_width
-
-
 def is_fp8_supported_gpu():
     if not torch.cuda.is_available():
         return False
